c4db6ba731e5a6324bff5b6a3a53c33f131f4045
An_Ger <60641394+An-Ger@users.noreply.github.com>
2020-06-15 11:46:02 +0800

    Add files via upload

---
+++ LeetCode/14.js
@@ -0,0 +1,14 @@
+var longestCommonPrefix = function(strs) {
+    strs.sort()//按编码排序
+    if (strs.length === 0) return ''//空数组返回''
+    var first = strs[0],
+        end = strs[strs.length - 1]
+    if(first === end || end.match(eval('/^' + first + '/'))){
+        return first//first包含于end返回first
+    }
+    for(var i=0;i<first.length;i++){
+        if(first[i] !== end[i]){
+            return first.substring(0,i)//匹配失败时返回相应字符串
+        }
+    }
+};
