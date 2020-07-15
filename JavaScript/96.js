b76f74a5dd2385189ccbdcc02e0f493810041ce8
an-ger <2455927306@qq.com>
2020-07-15 08:13:25 +0800

    卡特兰数

---
+++ JavaScript/96.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function(n) {
+    var res = 1;
+        for (let i = n + 1; i <= 2 * n; ++i) {
+            res = res * i / (i - n);
+        }
+        return res / (n + 1);
+};
