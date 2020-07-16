62be04b6c7cdaa8639e482beeaaf398513345684
an-ger <2455927306@qq.com>
2020-07-17 05:27:33 +0800

    35

---
+++ JavaScript/35.js
@@ -0,0 +1,9 @@
+
+var searchInsert = function(nums, target) {
+    if( nums.indexOf(target) != -1) return nums.indexOf(target)
+    for(var i=0; i<nums.length; i++){
+        if(target < nums[i]) return i
+    }
+    return i
+};
+
