c0ff2adefa3232f81c5d0a4532a2a36212c5933d
an-ger <2455927306@qq.com>
2020-07-24 07:07:17 +0800

    Dynamic Programming

---
+++ JavaScript/1025.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function (N) {
+  if (N == 1) {
+    return false;
+  }
+  if (N == 2) {
+    return true;
+  }
+  const dp = new Array(N + 1);
+  dp[1] = false;
+  dp[2] = true;
+  for (let i = 3; i <= N; i++) {
+    dp[i] = false;
+    for (let j = 1; j < i; j++) {
+      if (i % j === 0 && !dp[i - j]) {
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return dp[N];
+};
