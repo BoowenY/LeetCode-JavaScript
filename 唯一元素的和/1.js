2dd9bd0bc1a015543bf59ce94acd674f669fccf1
an-ger <aiboowen@gmail.com>
2021-02-06 23:55:02 +0800

    数组中唯一元素的和

---
+++ 唯一元素的和/1.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * 给你一个整数数组 nums 。数组中唯一元素是那些只出现 恰好一次 的元素。
+请你返回 nums 中唯一元素的 和 。
+示例 1：
+输入：nums = [1,2,3,2]
+输出：4
+解释：唯一元素为 [1,3] ，和为 4 。
+示例 2：
+输入：nums = [1,1,1,1,1]
+输出：0
+解释：没有唯一元素，和为 0 。
+示例 3 ：
+输入：nums = [1,2,3,4,5]
+输出：15
+解释：唯一元素为 [1,2,3,4,5] ，和为 15 。
+ */
+var sumOfUnique = function (nums) {
+  const n = nums.length;
+  let m = new Map();
+  let res = 0;
+  nums.map((e) => {
+    if (m.has(e)) {
+      let add = m.get(e);
+      add += 1;
+      m.set(e, add);
+    } else m.set(e, 1);
+  });
+  console.log(...m);
+  nums.map((e) => {
+    if (m.get(e) == 1) res += e;
+  });
+  return res;
+};
