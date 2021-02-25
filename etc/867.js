4e498af12a7c7094e162f502b5f73d69285a4e38
an-ger <aiboowen@gmail.com>
2021-02-25 22:31:00 +0800

    矩阵

---
+++ etc/867.js
@@ -0,0 +1,10 @@
+var transpose = function(matrix) {
+    const m = matrix.length, n = matrix[0].length;
+    const transposed = Array.from({length: matrix[0].length}, () => [])
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            transposed[j][i] = matrix[i][j];
+        }
+    }
+    return transposed;
+};
